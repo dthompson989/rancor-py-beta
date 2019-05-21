88c93428a6e7c8bcacd68363490a408d988ce216
David Thompson <d_m_tommy1@yahoo.com>
2019-05-21 13:50:55 -0400

    added main and supporting functions to az-scraper.py

--- lambda-az-scraper/az-scraper.py
+++ lambda-az-scraper/az-scraper.py
@@ -1,14 +1,25 @@
 #!usr/bin/python
-"""View detailed information about EC2 Instances with click and boto3"""
+"""View EC2 Instances Availability Zone break down using boto3"""
 import boto3
 
 ec2_client = boto3.client('ec2')
 s3_client = boto3.client('s3')
 
 
-def get_regions():
-    """A function to gather the regions"""
+def list_instances():
+    """A function to describe the instances in a given region"""
+    obj = ec2_client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['pending', 'running', 'stopped']}])
+
+    for reservation in obj["Reservations"]:
+        for instance in reservation["Instances"]:
+            print("{}: Availability Zone: {}".format(instance["InstanceId"], instance["Placement"]["AvailabilityZone"]))
 
 
-def handler(event, context):
+def lambda_handler(event, context):
     """The main function for an AWS Lambda function"""
+    list_instances()
+
+
+if __name__ == '__main__':
+    """Main function if running in the command line"""
+    list_instances()
