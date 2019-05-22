23d970a3edd74f22172ccb8034b0f97381c43b19
David Thompson <d_m_tommy1@yahoo.com>
2019-05-22 15:29:33 -0400

    added serverless handler.py

---
+++ serverless-functions/handler.py
@@ -0,0 +1,18 @@
+#!usr/bin/python
+"""A Lambda function that receives events from S3, Logs the event, and sends a notification to slack using boto3"""
+import os
+import requests
+
+
+def post_to_slack(event, content):
+    slack_url = os.environ['SLACK_WEBHOOK_URL']
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+    data = {"text": slack_message}
+
+    try:
+        response = requests.post(slack_url, json=data)
+        print(event + " Response Code: " + response.status_code)
+    except Exception as e:
+        print(event + " FAILED!!! {}".format(e))
+
+    return
