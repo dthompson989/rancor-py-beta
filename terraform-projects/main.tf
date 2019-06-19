63ada87a6f66a7c29a1a4c2e47f0286e4f5981f3
David Thompson <d_m_tommy1@yahoo.com>
2019-06-19 16:20:34 -0400

    Terraform Structure Changes

--- terraform-projects/main.tf
+++ terraform-projects/main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
+  profile    = var.profile
+  region     = var.region
 }
 
-resource "aws_instance" "main" {
+resource "aws_instance" "rancor-web01" {
   ami           = "ami-0c6b1d09930fac512"
   instance_type = "t2.micro"
-
-  vpc_security_group_ids = ["sg-05ab90c413f24716d"]
 }
