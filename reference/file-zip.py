f835621363af9c02f4302eadd65d4b1885b9cc25
Dave Thompson <d_m_t@IMP-DURLTDTHO1.4implus.local>
2019-08-14 14:59:53 -0400

    Added ZIP function

---
+++ reference/file-zip.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+"""A Python function that zips up a directory. Used with terraform-lambda/ """
+import shutil
+
+
+def zip_dir(zip_input, zip_output):
+    shutil.make_archive(zip_output, 'zip', zip_input)
+    print("{} -> has been ZIPPED -> {}".format(zip_input, zip_output))
+
+
+if __name__ == '__main__':
+    input_dir = input("What is the directory you want to ZIP? ")
+    output_name = input("What is the output ZIP file name?")
+    zip_dir(input_dir, output_name)
