a9a8c418366a3ad9b4fe55f92c61046b7620ee9a
David Thompson <d_m_tommy1@yahoo.com>
2019-08-29 12:08:44 -0400

    fixer serverless lambda

--- serverless-post-to-slack/handler.py
+++ serverless-post-to-slack/handler.py
@@ -1,5 +1,6 @@
 #!usr/bin/python
-"""A Lambda function that receives events from S3, Logs the event, and sends a notification to slack using serverless"""
+"""A Lambda function that receives events from AWS Services, Logs the event (in some way),
+   and sends a notification to slack. Built using Serverless"""
 import os
 import requests
 
@@ -10,10 +11,12 @@ def post_to_slack(event, context):
 
     """Try to post to slack, logging the successful or unsuccessful attempt"""
     try:
-        slack_message = "From {} {} {}".format(event['eventSource'], event['eventName'], event['object']['key'])
+        slack_message = "From {} {} {}".format(event['Records'][0]['eventSource'],
+                                               event['Records'][0]['eventName'],
+                                               event['Records'][0]['s3']['object']['key'])
         data = {"text": slack_message}
         response = requests.post(slack_url, json=data)
-        print("S3 LAMBDA Success Response Code: " + response.status_code)
+        print("S3 LAMBDA Success Response Code: {}".format(response.status_code))
     except Exception as e:
         print("S3 LAMBDA FAILED!!! Error: {} ::: Event {}".format(e, event))
 
