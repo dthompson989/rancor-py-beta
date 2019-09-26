f8fe2d53e911e54bfdddc58e66a64e8438b342e1
David Thompson <d_m_tommy1@yahoo.com>
2019-09-26 10:00:51 -0400

    changes to serverless post to slack test_handler.py function

--- serverless-post-to-slack/unit_tests/test_handler.py
+++ serverless-post-to-slack/unit_tests/test_handler.py
@@ -3,12 +3,17 @@
 import boto3
 import pytest
 from botocore.exceptions import ClientError
-from handler import post_to_slack
+import importlib.util
 from moto import mock_s3
+
+"""Import the post to slack module"""
+spec = importlib.util.spec_from_file_location('post_to_slack', '../handler.py')
+slack_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(slack_module)
 
 
 @mock_s3
 def test_post_to_slack():
     """The lambda handler test function"""
-    post_to_slack(open('unit_tests/s3_test.json', 'r').read(), "context")
-    post_to_slack(open('unit_tests/sns_test.json', 'r').read(), "context")
+    slack_module.post_to_slack(open('unit_tests/s3_test.json', 'r').read(), "context")
+    slack_module.post_to_slack(open('unit_tests/sns_test.json', 'r').read(), "context")
