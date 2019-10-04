541db602d5f337c6f1258f16d6ce7acae61f7f1c
David Thompson <d_m_tommy1@yahoo.com>
2019-10-04 13:10:44 -0400

    added lambda arn

--- terraform-sns/sns.tf
+++ terraform-sns/sns.tf
@@ -6,3 +6,9 @@ resource "aws_sns_topic_policy" "rancor-sns-policy" {
   arn    = aws_sns_topic.rancor-sns.arn
   policy = file("rancor-sns-policy.json")
 }
+
+resource "aws_sns_topic_subscription" "rancor-sns-lambda-subscription" {
+  topic_arn = aws_sns_topic.rancor-sns.arn
+  protocol  = "lambda"
+  endpoint  = var.lambda_arn
+}
--- terraform-sns/vars.tf
+++ terraform-sns/vars.tf
@@ -1,3 +1,5 @@
 # Provider
 variable "profile" {}
 variable "region" { default = "us-east-2" }
+# Lambda ARN
+variable "lambda_arn" {}
