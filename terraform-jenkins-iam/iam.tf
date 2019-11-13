ebe9f4fe6e1eaa1ad9c69ae2485c56284f57cd47
David Thompson <d_m_tommy1@yahoo.com>
2019-11-13 10:08:58 -0500

    added additional permissions to terraform-jenkins-iam

--- terraform-jenkins-iam/iam.tf
+++ terraform-jenkins-iam/iam.tf
@@ -3,14 +3,10 @@
 data "aws_iam_policy_document" "jenkins-assume-role-policy-document" {
   policy_id = "__assume_role_policy_ID"
   statement {
-    actions = [
-      "sts:AssumeRole"
-    ]
+    actions = ["sts:AssumeRole"]
     principals {
-      type = "Service"
-      identifiers = [
-        "ec2.amazonaws.com"
-      ]
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
     }
   }
 }
@@ -18,17 +14,21 @@ data "aws_iam_policy_document" "jenkins-assume-role-policy-document" {
 # This is the IAM Additional Permissions Document
 data "aws_iam_policy_document" "jenkins-additional-policy-document" {
   policy_id = "__default_jenkins_policy_ID"
-
+  statement {
+    actions   = ["sns:Publish"]
+    resources = [var.sns_arn]
+  }
+  statement {
+    actions   =["logs:CreateLogStream"]
+    resources = [var.cw_log_stream_arn]
+  }
+  statement {
+    actions   =["logs:PutLogEvents"]
+    resources = [var.cw_put_log_arn]
+  }
 }
 
 ################# IAM POLICIES #################
-# This is the Assume Role Policy
-resource "aws_iam_policy" "jenkins-assume-role-policy" {
-  name   = "jenkins-assume-role-policy"
-  path   = "/"
-  policy = data.aws_iam_policy_document.jenkins-assume-role-policy-document.json
-}
-
 # This is the Additional Permissions Policy
 resource "aws_iam_policy" "jenkins-additional-policy" {
   name   = "jenkins-additional-policy"
@@ -39,20 +39,18 @@ resource "aws_iam_policy" "jenkins-additional-policy" {
 ################# IAM ROLE #################
 resource "aws_iam_role" "rancor-jenkins-role" {
   name               = "rancor-jenkins-role"
-  assume_role_policy = aws_iam_policy.jenkins-assume-role-policy.name
+  assume_role_policy = data.aws_iam_policy_document.jenkins-assume-role-policy-document.json
 }
 
 ################# IAM ROLE POLICY ATTACHMENTS  #################
 # Attach the Default SSM Policy
 resource "aws_iam_role_policy_attachment" "jenkins-default-policy-attach" {
-  name       = "jenkins-default-policy-attach"
   role       = aws_iam_role.rancor-jenkins-role.name
   policy_arn = var.jenkins_default_policy_arn
 }
 
 # Attach the Additional Permissions Policy
 resource "aws_iam_role_policy_attachment" "jenkins-additional-policy-attach" {
-  name       = "jenkins-additional-policy-attach"
   role       = aws_iam_role.rancor-jenkins-role.name
   policy_arn = aws_iam_policy.jenkins-additional-policy.arn
 }
--- terraform-jenkins-iam/vars.tf
+++ terraform-jenkins-iam/vars.tf
@@ -3,3 +3,8 @@ variable "profile" {}
 variable "region" { default = "us-east-2" }
 # IAM
 variable jenkins_default_policy_arn {}
+# SNS ARN
+variable sns_arn {}
+# CloudWatch
+variable cw_log_stream_arn {}
+variable cw_put_log_arn {}
