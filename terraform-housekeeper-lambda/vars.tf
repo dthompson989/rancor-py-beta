47fc3a22d2a74f51dc8de39469ca68e77f3be8ed
David Thompson <d_m_tommy1@yahoo.com>
2022-02-04 20:54:57 -0500

    fixed AWS CW cron expression and fixed pipeline.sh

--- terraform-housekeeper-lambda/vars.tf
+++ terraform-housekeeper-lambda/vars.tf
@@ -6,8 +6,6 @@ variable "lamdba_runtime" {}
 variable "lambda_handler" {}
 variable "lambda_filename" {}
 variable "lambda_function" {}
-# SNS ARN
-variable "sns_arn" {}
 # CloudWatch ARN's
 variable cw_log_stream_arn {}
 variable cw_put_log_arn {}
