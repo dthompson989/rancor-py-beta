a3e10b885a0fff2fbd988561e18816a11c9675a2
David Thompson <d_m_tommy1@yahoo.com>
2022-04-08 10:05:14 -0400

    added multi region checking to housekeeper lambda

--- todo.py
+++ todo.py
@@ -2,5 +2,5 @@
 # TODO: Lock down dev website to only "internal" traffic. - Mostly Done
 # TODO: Add website support to pipeline.sh
 #       TODO: Include security scanning tools and linting tools
-# TODO: add terraform version solution (tfswitch)
-# TODO: Add prod website "ping" to housekeeper lambda
+# TODO: add terraform version solution (tfswitch) - DONE
+# TODO: Add prod website "ping" automation
